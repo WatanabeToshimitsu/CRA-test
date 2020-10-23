7c5ffb0253615f6ce1667394c3e06c50c5c01f64
Toshimitsu Watanabe <toshimitsu.watanabe@tier4.jp>
2020-10-23 17:42:58 +0900

    add annimation when erase charactors, etc

--- front-root/src/App.js
+++ front-root/src/App.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring'
-import './App.css';
+import './App.scss';
 
 function App() {
   const [chars, setChars] = React.useState([]);
+  const [isMorning, setIsMorning] = React.useState(true);
 
   const addChars = React.useCallback((char) => setChars((prev) => [char, ...prev]), [setChars]);
+  const toggleMorning = React.useCallback(()=> setIsMorning((prev) => !prev), [setIsMorning]);
 
   return (
-    <div className="App">
-        <CharGenerator chars={chars} addChars={addChars} />
+    <div className={"App" + (isMorning ? "__Morning" : "__Night")}>
+      <CharGenerator chars={chars} addChars={addChars} />
+      <button
+        onClick={toggleMorning}
+        className={"ThemeToggle" + (isMorning ? "__Morning" : "__Night")}
+      >
+        {isMorning ? "Turn to night" : "Turn to morning"}
+      </button>
     </div>
   )
 }
 
 function CharGenerator(props) {
-  const randomAreaWidth = 95;
-  const randomAreaHight = 80;
-  const top = Math.random() * randomAreaHight -2;
-  const left = Math.random() * randomAreaWidth - 2;
+  const randomAreaWidth = 98;
+  const randomAreaHight = 95;
+  const top = Math.random() * randomAreaHight;
+  const left = Math.random() * randomAreaWidth;
   
-  const minFontSize = 5
-  const maxFontSize = 20
+  const minFontSize = 0.5
+  const maxFontSize = 5
   const fontSizeRatio = Math.random();
   const fontsize = fontSizeRatio * maxFontSize + (1 - fontSizeRatio) * minFontSize;
   
   const randNumIn256 = () => (Math.round(Math.random() * 256));
-
   const red = randNumIn256().toString(16); 
   const green = randNumIn256().toString(16); 
   const blue = randNumIn256().toString(16); 
-
   const rgb = "#" + red + green + blue;
 
   const generateChar = (e) => {
@@ -45,8 +51,8 @@ function CharGenerator(props) {
   }
   return (
     <div>
-      <p className={"Title"} onKeyPress={generateChar} tabIndex={0}>Generate Charactors</p>
-      <CharCluster chars={props.chars} />
+        <p className={"Title"} onKeyPress={generateChar} tabIndex={0}>Generate Charactors</p>
+        <CharCluster chars={props.chars} />
     </div>
 
   )
@@ -75,23 +81,27 @@ function CharCluster(props) {
 
 function Char(props) {
   const [existing, setExisting] = React.useState(true);
-  const erase = React.useCallback(() => setExisting((prev) => !prev), [setExisting]);
-
-  const style = {
+  const erase = React.useCallback(() => setExisting(false), [setExisting]);
+  
+  const spring = useSpring({
+    fontSize: `${props.fontsize}em`,
+    opacity: existing ? 0.8 : 0,
     position: "absolute",
     top: `${props.top}vh`,
     left: `${props.left}vw`,
-    fontSize: `${props.fontsize}vmin`,
     color: `${props.rgb}`,
-    margin: 0
-  };
+    margin: 0,
+    transform: `translate(-50%, -50%)`
+  });
 
-  if (existing) {
-    return <p style={style} onMouseOver={erase}>{props.char}</p>
-  }
-  else {
-    return false
-  }
+  return (
+      <animated.div
+        style={spring}
+        onMouseEnter={erase}
+      >
+      {props.char}
+      </animated.div>
+    )
 }
 
 export default App;
