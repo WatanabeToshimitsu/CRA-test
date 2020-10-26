a000a1d95bbd945462809ed8fb6d03f524b64161
Toshimitsu Watanabe <toshimitsu.watanabe@tier4.jp>
2020-10-26 20:43:24 +0900

    change root directory

---
+++ build/precache-manifest.5459f1a3438181fd44a088d37f887edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c910232998841bbe913e15301254dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a89652091bed6cc0f7",
+    "url": "/static/css/main.61f8b2fc.chunk.css"
+  },
+  {
+    "revision": "fc5ce2d65fcc88dee004",
+    "url": "/static/js/2.2a67160a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.2a67160a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a89652091bed6cc0f7",
+    "url": "/static/js/main.1fa7f46b.chunk.js"
+  },
+  {
+    "revision": "435c84664e55830b69df",
+    "url": "/static/js/runtime-main.2dffbe6b.js"
+  }
+]);
